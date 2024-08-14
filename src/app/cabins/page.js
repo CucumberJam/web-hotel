8f7b040ac4ba94d9bd472a1bb4731fffe1031458
Kseniya Volodar <cucumber12@bk.ru>
2024-08-14 11:38:56 +0500

    cabin-list ui/done

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,11 +1,11 @@
-import CabinCard from "@/app/_components/CabinCard.js";
-import {getCabins} from "@/app/_lib/data-service.js";
+import CabinList from "@/app/_components/CabinList.js";
+import {Suspense} from "react";
+import Spinner from "@/app/_components/Spinner.js";
 
 export const metadata = {
     title: 'Cabins'
 }
 export default async function Page(){
-    const cabins = await getCabins();
     return (
         <div>
             <h1 className="mb-5 text-accent-400 font-medium
@@ -21,14 +21,9 @@ export default async function Page(){
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
-
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Spinner/>}>
+                <CabinList/>
+            </Suspense>
         </div>
     );
 }
