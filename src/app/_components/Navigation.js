4bbfc002f5f8d61f4abb4d852242c04c067dc625
Kseniya Volodar <cucumber12@bk.ru>
2024-08-18 16:31:43 +0500

    auth-user-creds added to ui-render/done

--- src/app/_components/Navigation.js
+++ src/app/_components/Navigation.js
@@ -1,6 +1,8 @@
 import Link from "next/link.js";
+import {auth} from "@/app/_lib/auth.js";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex
@@ -18,9 +20,19 @@ export default function Navigation() {
           </Link>
         </li>
         <li className='h-5 sm:h-fit'>
-          <Link href="/account" className="text-sm sm:text-base md:text-xl hover:text-accent-400 transition-colors">
-            Account
-          </Link>
+          {session?.user?.image ? (
+              <Link href="/account" className="flex items-center gap-4 text-sm sm:text-base md:text-xl hover:text-accent-400 transition-colors">
+                <img className='h-8 rounded-full'
+                     src={session.user.image}
+                     alt={session.user.name}
+                     referrerPolicy={"no-referrer"}/>
+                <span>Account</span>
+              </Link>
+              ):
+              <Link href="/account" className="text-sm sm:text-base md:text-xl hover:text-accent-400 transition-colors">
+                Account
+              </Link>
+          }
         </li>
       </ul>
     </nav>
