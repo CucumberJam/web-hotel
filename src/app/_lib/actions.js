96e9f3dd51e05253c40d4f4248ec6872abe91642
Kseniya Volodar <cucumber12@bk.ru>
2024-10-26 17:18:28 +0500

    update-reservation server-action/done

--- src/app/_lib/actions.js
+++ src/app/_lib/actions.js
@@ -3,9 +3,10 @@ import {auth, signIn, signOut} from '@/auth.js'
 import bcrypt from "bcryptjs";
 import {userData} from "@/app/_lib/constants.js";
 import validateData from "@/app/_lib/validateHelper.js";
-import {getBookings, updateGuest} from "@/app/_lib/data-service.js";
+import {getBookings, updateGuest, updateBooking as fetchUpdateBooking} from "@/app/_lib/data-service.js";
 import {revalidatePath} from "next/cache";
 import {supabase} from "@/app/_lib/supabase.js";
+import {redirect} from "next/navigation";
 
 export async function signInAction(formData){
     const action = formData.get('action');
@@ -57,28 +58,59 @@ export async function updateGuestAction(formData = null){
         await updateGuest(session.user.guestId, updatedParams);
         revalidatePath('/account/profile', 'page');
     }catch (e) {
-        throw new Error(e);
+        throw new Error(e.message);
     }
 }
 
 export async function deleteReservation(bookingId){
+    try{
+        const guestId = await checkUserSession(bookingId);
+        if(guestId){
+            const { error } = await supabase.
+            from('bookings').
+            delete().
+            eq('id', bookingId).
+            eq("guestId", guestId);
+
+            if (error) {
+                console.error(error);
+                throw new Error('Booking could not be deleted');
+            }
+            revalidatePath('/account/reservations');
+        } else throw new Error('No authorized guest with this id found');
+    }catch (e) {
+        throw new Error(e.message);
+    }
+
+}
+export async function updateBooking(formData = null){
+    let data = null;
+    const bookingId = Number(formData.get('bookingId'));
+    try{
+        const guestId = await checkUserSession(bookingId, 'update');
+        if(guestId){
+            const updateData = {
+                numGuests: Number(formData.get('numGuests')),
+                observations: formData.get('observations').slice(0, 1000)
+            }
+            data = await fetchUpdateBooking(bookingId, updateData);
+        }else throw new Error('No authorized guest with this id found');
+    }catch (e) {
+        throw new Error(e.message);
+    }
+    if(bookingId === data?.id) revalidatePath('/account/reservations', 'layout');
+    redirect('/account/reservations');
+}
+async function checkUserSession(bookingId, message = 'delete'){
+    // Authentication:
     const session = await auth();
     if(!session) throw new Error('You must be logged in');
 
+    // Authorization:
     const bookings = await getBookings(session.user.guestId);
-    if (!bookings.some((el) => el.id === +bookingId)) {
-        throw new Error("You are not allowed to delete this booking");
+    if (!bookings.some((el) => el.id === bookingId)) {
+        throw new Error(`You are not allowed to ${message} this booking`);
     }
 
-    const { error } = await supabase.
-    from('bookings').
-    delete().
-    eq('id', bookingId).
-    eq("guestId", session.user.guestId);
-
-    if (error) {
-        console.error(error);
-        throw new Error('Booking could not be deleted');
-    }
-    revalidatePath('/account/reservations');
+    return session?.user?.guestId;
 }
