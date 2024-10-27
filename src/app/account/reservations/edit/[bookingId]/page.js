56e9c1ef6e4905c6af0d5be1ca077a2eb8a03758
Kseniya Volodar <cucumber12@bk.ru>
2024-10-27 20:20:53 +0500

    edit-reservation styles for phone/fixed

--- src/app/account/reservations/edit/[bookingId]/page.js
+++ src/app/account/reservations/edit/[bookingId]/page.js
@@ -10,11 +10,16 @@ export default async function Page({params}) {
 
   return (
     <div>
-      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      <h2 className="font-semibold text-accent-400 mb-7
+      text-base md:text-lg lg:text-2xl">
         Edit Reservation #{bookingId}
       </h2>
 
-      <form action={updateBooking} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form action={updateBooking} className="bg-primary-900 flex flex-col
+      py-4 md:py-6 lg:py-8
+      px-6 md:px-8 lg:px-12
+      text-sm md:text-base lg:text-lg
+      gap-3 md:gap-5 lg:gap-6">
         <input type='hidden' value={bookingId} name='bookingId'/>
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
@@ -22,7 +27,9 @@ export default async function Page({params}) {
             name="numGuests"
             id="numGuests"
             defaultValue={numGuest}
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
+            className="bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm
+            px-2 md:px-3 lg:px-5
+            py-1 md:py-2 lg:py-3 "
             required>
             <option value="" key="">
               Select number of guests...
@@ -42,10 +49,12 @@ export default async function Page({params}) {
           <textarea
             name="observations"
             defaultValue={observations}
-            className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"/>
+            className="bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm
+            px-2 md:px-3 lg:px-5
+            py-1 md:py-2 lg:py-3 "/>
         </div>
 
-        <SubmitButton labelLoading='Update reservation'/>
+        <SubmitButton  labelLoading='Update reservation'/>
       </form>
     </div>
   );
